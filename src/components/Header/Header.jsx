cdcbf9bd9cab1c624e10b79cb0ba13227bacfc94
YoshidaReo <happypan86@gmail.com>
2023-05-28 16:00:40 +0900

    ✨ HeaderとMainの構成に変更しました。

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,153 +1,19 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-// import styles from '@/styles/Home.module.css'
 import classes from '@/components/Header/Header.module.css'
-import { useCallback, useState } from 'react'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export function Header() {
-  const [foo, setFoo] = useState(0);
-  const [state,setState] = useState([]);
-  // 更新用の空配列、オブジェクトを使用する。
-  const message = [];
-  const obj ={};
-
-
-  const initialState = {
-    id:foo,
-    name:'新規作成'+String(foo),
-    // 元本
-    principal: 10, 
-    // 毎月積立金額
-    monthlyMoney: 2,
-    // 年利
-    annualInterest: 4,
-    // 増減配当率
-    IncreaseDecreaseRate: 3,
-    // コスト年率
-    costAnnualRate: 1,
-    // 投資期間
-    investmentPeriod: 10
-  };
-
-
-
-  const handleAdd = useCallback((e) => {
-    setFoo((prevFoo) => prevFoo + 1);
-    
-    setState((prevState) => [...prevState, initialState]);
-  },[foo]);
-
-
-
-  const handleChange = useCallback((prevItems, target) => {
-    // console.log(prevItems);
-
-    state.map((items) => {
-      if (items.id === prevItems.id) {
-        message.push({...items, [target.name] : target.value});
-
-      } else {
-        message.push(items);
-      }
-    });
-    // console.log(message);
-    setState((state) => message);
-
-    return {state};
-    
-  },[state]);
-
-
-
   return (
     <>
-      {state.map((items,index) => {
-        return (
-        <div key={items.id} className={classes.data}>
-
-          <h4>
-            {items.name}
-          </h4>
-          <ul className={classes.items}>
-              {/* onChangeに関数でpropsを渡すことで親コンポーネントを引き継ぐことができる。 */}
-              {/* nameを使用することで、e.target.nameで照合することができる。 */}
-              <li>
-                <div className={classes.item}>
-                  <p>名前</p>
-                  <input type="text" name="name" value={items.name} onChange={(e) => handleChange(items, e.target)} />
-                </div>
-              </li>
-              <li>
-                <div className={classes.item}>
-                  <p>元本</p>
-                  <input type="text" name="principal" value={items.principal} onChange={(e) => handleChange(items, e.target)} />
-                </div>
-              </li>
-              <li>
-                <div className={classes.item}>
-                  <p>毎月積立金額</p>
-                  <input type="text" name="monthlyMoney" value={items.monthlyMoney} onChange={(e) => handleChange(items, e.target)} />
-                </div>
-              </li>
-              <li>
-                <div className={classes.item}>
-                  <p>年利</p>
-                  <input type="text" name="annualInterest" value={items.annualInterest} onChange={(e) => handleChange(items, e.target)} />
-                </div>
-              </li>
-              <li>
-                <div className={classes.item}>
-                  <p>増減配当率</p>
-                  <input type="text" name="IncreaseDecreaseRate" value={items.IncreaseDecreaseRate} onChange={(e) => handleChange(items, e.target)} />
-                </div>
-              </li>
-              <li>
-                <div className={classes.item}>
-                  <p>コスト年率</p>
-                  <input type="text" name="costAnnualRate" value={items.costAnnualRate} onChange={(e) => handleChange(items, e.target)} />
-                </div>
-              </li>
-              <li>
-                <div className={classes.item}>
-                  <p>投資期間</p>
-                  <input type="text" name="investmentPeriod" value={items.investmentPeriod} onChange={(e) => handleChange(items, e.target)} />
-                </div>
-              </li>
-          </ul>
-        </div>
-        );
-      })}
-
-      <button onClick={handleAdd}>
-        新規追加
-      </button>
-
-
-      {/* <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/pages/index.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div> */}
+      <div className={classes.header}>
+        <h2>計算一覧</h2>
+        <Link href="#" className={classes.inner}>
+          設定
+        </Link>
+      </div>
     </>
   )
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,127 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import classes from '@/components/Main/Main.module.css'
+import { useCallback, useState } from 'react'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export function Main() {
+  const [foo, setFoo] = useState(0);
+  const [state,setState] = useState([]);
+  // 更新用の空配列、オブジェクトを使用する。
+  const message = [];
+  const obj ={};
+
+
+  const initialState = {
+    id:foo,
+    name:'新規作成'+String(foo),
+    // 元本
+    principal: 100000, 
+    // 毎月積立金額
+    monthlyMoney: 2000,
+    // 年利
+    annualInterest: 4,
+    // 増減配当率
+    IncreaseDecreaseRate: 3,
+    // コスト年率
+    costAnnualRate: 1,
+    // 投資期間
+    investmentPeriod: 10
+  };
+
+
+
+  const handleAdd = useCallback((e) => {
+    setFoo((prevFoo) => prevFoo + 1);
+    
+    setState((prevState) => [...prevState, initialState]);
+  },[foo]);
+
+
+
+  const handleChange = useCallback((prevItems, target) => {
+    // console.log(prevItems);
+
+    state.map((items) => {
+      if (items.id === prevItems.id) {
+        message.push({...items, [target.name] : target.value});
+
+      } else {
+        message.push(items);
+      }
+    });
+    // console.log(message);
+    setState((state) => message);
+
+    return {state};
+    
+  },[state]);
+
+
+
+  return (
+    <>
+      {state.map((items,index) => {
+        return (
+        <div key={items.id} className={classes.data}>
+
+          <h4>
+            {items.name}
+          </h4>
+          <ul className={classes.items}>
+              {/* onChangeに関数でpropsを渡すことで親コンポーネントを引き継ぐことができる。 */}
+              {/* nameを使用することで、e.target.nameで照合することができる。 */}
+              <li className={classes.li}>
+                <div className={classes.item}>
+                  <p>名前</p>
+                  <input type="text" name="name" value={items.name} onChange={(e) => handleChange(items, e.target)} />
+                </div>
+              </li>
+              <li>
+                <div className={classes.item}>
+                  <p>元本</p>
+                  <input type="text" name="principal" value={items.principal} onChange={(e) => handleChange(items, e.target)} />
+                </div>
+              </li>
+              <li>
+                <div className={classes.item}>
+                  <p>毎月積立金額</p>
+                  <input type="text" name="monthlyMoney" value={items.monthlyMoney} onChange={(e) => handleChange(items, e.target)} />
+                </div>
+              </li>
+              <li>
+                <div className={classes.item}>
+                  <p>年利</p>
+                  <input type="text" name="annualInterest" value={items.annualInterest} onChange={(e) => handleChange(items, e.target)} />
+                </div>
+              </li>
+              <li>
+                <div className={classes.item}>
+                  <p>増減配当率</p>
+                  <input type="text" name="IncreaseDecreaseRate" value={items.IncreaseDecreaseRate} onChange={(e) => handleChange(items, e.target)} />
+                </div>
+              </li>
+              <li>
+                <div className={classes.item}>
+                  <p>コスト年率</p>
+                  <input type="text" name="costAnnualRate" value={items.costAnnualRate} onChange={(e) => handleChange(items, e.target)} />
+                </div>
+              </li>
+              <li>
+                <div className={classes.item}>
+                  <p>投資期間</p>
+                  <input type="text" name="investmentPeriod" value={items.investmentPeriod} onChange={(e) => handleChange(items, e.target)} />
+                </div>
+              </li>
+          </ul>
+        </div>
+        );
+      })}
+
+      <button onClick={handleAdd} className={classes.btn}>
+        新規追加
+      </button>
+    </>
+  )
+}
