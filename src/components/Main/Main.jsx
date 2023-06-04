ed715118e28d9bb777ec0b408d15af4d75c9606e
YoshidaReo <happypan86@gmail.com>
2023-06-05 07:17:59 +0900

    ✨ 経過年数をkeyにしました。

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -217,7 +217,7 @@ export function Main() {
                   <p>最終資産額:{items.finalAsset}</p>
                   <p>最終利益:{items.bottomLine}</p>
                   <p>総資産利益率:{items.returnOnAssets}%</p>
-                  {/* グラフ作成用
+                  {/* グラフ作成用 */}
                   <div>
                     <table>
                       <thead>
@@ -231,7 +231,7 @@ export function Main() {
                     <tbody>
                         {table.map((items) => {
                           return (
-                            <tr>
+                            <tr key={items.year}>
                               <td>{items.year}年</td>
                               <td>{items.totalInvestmentResult}円</td>
                               <td>{items.finalAssetResult}円</td>
@@ -241,7 +241,7 @@ export function Main() {
 
                     </tbody>
 
-                  </div> */}
+                  </div>
                 </div>
               ) : (
                 <div>入力してください</div>
