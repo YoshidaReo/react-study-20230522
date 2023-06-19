4d88c10e701a295dba431ffd30b15d52e5ebbd44
YoshidaReo <happypan86@gmail.com>
2023-06-20 07:05:42 +0900

    ⚡️ 棒グラフの色を変えました。

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,41 +1,50 @@
 import { Inter } from 'next/font/google'
 import classes from '@/components/Chart/Chart.module.css'
 import React from "react";
+// import {
+//   Chart as ChartJS,
+//   LineElement,
+//   PointElement,
+//   CategoryScale,
+//   LinearScale,
+//   Tooltip,
+//   Legend
+// } from "chart.js";
+
+// import { Chart, Bar, Line } from "react-chartjs-2";
+// // import {  } from "react-chartjs-2";
+
+// ChartJS.register (
+//   LineElement,
+//   PointElement,
+//   CategoryScale,
+//   LinearScale,
+//   Tooltip,
+//   Legend
+// );
+
 import {
   Chart as ChartJS,
-  LineElement,
-  PointElement,
   CategoryScale,
   LinearScale,
+  BarElement,
+  Title,
   Tooltip,
   Legend
 } from "chart.js";
+import { Bar } from "react-chartjs-2";
 
-import { Chart, Bar, Line } from "react-chartjs-2";
-// import {  } from "react-chartjs-2";
-
-ChartJS.register (
-  LineElement,
-  PointElement,
+ChartJS.register(
   CategoryScale,
   LinearScale,
+  BarElement,
+  Title,
   Tooltip,
   Legend
 );
 
 export function ChartDisplay(props) {
 
-
-
-  // console.log(props);
-  // console.log(...props.table.map(item => item["year"]));
-  // console.log(...props.table.map(item => item["year"]));
-  // let labels = ["1 月", "2 月", "3 月", "4 月", "5 月", "6 月"];
-  // 横軸
-  
-  // if (props.items["id"] === 1) {
-  //   return;
-  // };
 
   let labels = [];
   labels.push(...props.table.map(
@@ -78,25 +87,26 @@ export function ChartDisplay(props) {
         label: "資産額",
         // data: [65, 59, 60, 81, 56, 55],
         data: assetResult,
-        borderColor: "rgb(75, 192, 192)",
+        backgroundColor: "rgb(75, 192, 192)",
       },
       {
         // type: Line,
         label: "投資額",
         data: investmentResult,
-        borderColor: "rgb(75, 100, 192)",
+        backgroundColor: "rgb(75, 100, 192)",
       },
       {
         // type: Bar,
         label: "利益額",
         data: bottomLineResult,
-        borderColor: "rgb(192, 100, 75)",
+        backgroundColor: "rgb(192, 100, 75)",
       },
       {
         // type: Line,
         label: "利益率",
         data: returnOnAssetsResult,
-        borderColor: "rgb(75, 192, 100)",
+        // borderColor: "rgb(75, 192, 100)",
+        backgroundColor: "rgb(75, 192, 100)",
       },
     ]
   };
@@ -105,16 +115,15 @@ export function ChartDisplay(props) {
     <div className={classes.div}>
       {/* <h3>グラフ表示</h3> */}
       <div>
-        <Line
+        <Bar
           data = {data}
           // options = {options}
           // redraw
         
-        ></Line>
+        ></Bar>
       </div>
     </div>
   )
 
-
-
 }
+
